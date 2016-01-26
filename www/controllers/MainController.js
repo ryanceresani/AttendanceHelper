af07a408837c2dc04a1bff761b004f92ac519bb4
Jimbo73 <z97249@gmail.com>
2016-01-26 00:53:50 -0500

    initial

---
+++ www/controllers/MainController.js
@@ -0,0 +1,38 @@
+﻿angular.module('starter')
+.controller('MainController', ['$scope', function ($scope) {
+    $scope.classes = [
+        {
+            name: 'class1'
+        },
+        {
+            name: 'class2'
+        },
+        {
+            name: 'discrete'
+        },
+        {
+            name: 'more like borophyl'
+        },
+        {
+            name: 'class1'
+        },
+        {
+            name: 'class2'
+        },
+        {
+            name: 'discrete'
+        },
+        {
+            name: 'more like borophyl'
+        },
+        {
+            name: 'more like borophyl'
+        },
+        {
+            name: 'more like borophyl'
+        },
+        {
+            name: 'more like borophyl'
+        }
+    ]
+}]);
